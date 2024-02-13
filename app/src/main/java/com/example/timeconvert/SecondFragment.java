22ea1d83a1b424f58edadd6c2ed604c46776e3a4
ericzhy0815 <69284146+ericzhy0815@users.noreply.github.com>
2024-02-13 14:23:35 -0500

    Updated drop down option

--- app/src/main/java/com/example/timeconvert/SecondFragment.java
+++ app/src/main/java/com/example/timeconvert/SecondFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,9 +15,12 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.timeconvert.databinding.FragmentSecondBinding;
 
+import java.util.ArrayList;
+
 public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
+    private Spinner spinner_locations;
 
     @Override
     public View onCreateView(
@@ -28,14 +35,30 @@ public class SecondFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        spinner_locations = getView().findViewById(R.id.spinner_location);
+        spinner_locations.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getContext(), "Selected Item: "+item, Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
+        ArrayList<String> locations = new ArrayList<>();
+        locations.add("US");
+        locations.add("China");
+        locations.add("Canada");
+        locations.add("Mexico");
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, locations);
+        adapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        spinner_locations.setAdapter(adapter);
+
     }
 
     @Override
