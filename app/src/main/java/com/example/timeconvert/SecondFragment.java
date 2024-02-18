5bcdc4482fe6fb277329e6e8e34aec18ce0c6679
ericzhy0815 <69284146+ericzhy0815@users.noreply.github.com>
2024-02-17 21:08:24 -0500

    Updated settings

--- app/src/main/java/com/example/timeconvert/SecondFragment.java
+++ app/src/main/java/com/example/timeconvert/SecondFragment.java
@@ -1,5 +1,6 @@
 package com.example.timeconvert;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,9 +8,10 @@ import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
@@ -23,16 +25,22 @@ public class SecondFragment extends Fragment implements AdapterView.OnItemSelect
     private FragmentSecondBinding binding;
     private Spinner spinner_locations;
 
+    private EditText userInputEditText;
+    private Button saveButton;
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String KEY_USER_TEXT = "HomeCity";
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
         binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
+
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -63,6 +71,33 @@ public class SecondFragment extends Fragment implements AdapterView.OnItemSelect
         adapter.setDropDownViewResource(android.R.layout.select_dialog_item);
         spinner_locations.setAdapter(adapter);
 
+        saveButton = getView().findViewById(R.id.save_button);
+        userInputEditText = getView().findViewById(R.id.home_city_input);
+        loadUserText();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveUserData();
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+
+
+    }
+
+    private void saveUserData() {
+        // Get user input from the EditText
+        String userInput = userInputEditText.getText().toString();
+
+        // Save edit text input permanently
+        SharedPreferences preferences = requireActivity().getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_USER_TEXT, userInput);
+        editor.apply();
+
+        Toast.makeText(requireContext(), "Saved!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -75,10 +110,22 @@ public class SecondFragment extends Fragment implements AdapterView.OnItemSelect
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (parent.getId() == R.id.spinner_location) {
             String value = parent.getItemAtPosition(position).toString();
-
+            SharedPreferences.Editor editor = this.getContext().getSharedPreferences("MyPref", 0).edit();
+            editor.putString("userInputKey", value);
         }
     }
 
+    private void loadUserText() {
+        // Load previously saved text from SharedPreferences
+        SharedPreferences preferences = requireActivity().getSharedPreferences(PREFS_NAME, 0);
+        String savedUserText = preferences.getString(KEY_USER_TEXT, "");
+
+        if (savedUserText.isEmpty()) {
+            userInputEditText.setText("Baltimore");
+        } else {// Display the loaded text in the EditText
+            userInputEditText.setText(savedUserText);
+        }
+    }
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
